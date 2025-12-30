652a58d1c293ed76d02c20ced043b70fc7a96d8b
aaron moroney <aaron.createdesign@gmail.com>
2025-12-30 22:19:22 +0000

    feat(carousel): Carousel implemented on hero

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "./views");
-
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("index");
